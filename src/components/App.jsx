9b444c1955c580dfb810b0660e0b9a7e2717ed02
Aleksandr Pavlov <pavlovaleksandr@ukr.net>
2022-09-07 21:13:45 +0300

    createStore and connect store with code. 1:54:01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,50 @@
-import React, { useState, useEffect} from "react";
+import React from "react";
 import ContactsForm from "./ContactForm/ContactForm";
 import Filter from "./Filter/Filter";
 import ContactsList from "./ContactList/ContactList";
-import { nanoid } from "nanoid";
+// import { nanoid } from "nanoid";
 import css from "./App.module.css";
+
+import { useSelector } from "react-redux";
 
 
 
 const App = () => {
-  const[contacts, setContacts] = useState([
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ]);
-  const [filter, setFilter] = useState('');
+
+  const contacts = useSelector((store) => store.contacts.items);
+  const filter = useSelector((store) => store.contacts.filter);
+ 
+  
+  // const[contacts, setContacts] = useState([
+  //   {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+  //   {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+  //   {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+  //   {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  // ]);
+  // const [filter, setFilter] = useState('');
 
   
- const onFilterChange = (e) => {
-    setFilter(e.target.value);
-  }
+//  const onFilterChange = (e) => {
+//     setFilter(e.target.value);
+//   }
 
-  const deleteContact = (id) => {
-    const newContacts = contacts.filter(contact => contact.id !== id);
-    setContacts(newContacts);
-  }
-  const fomSubmitHeader = (contact) => {
-    const newContact = {
-      id: nanoid(),
-      name: contact.name,
-      number: contact.number,
-    };
-    setContacts([...contacts, newContact]);
-    console.log(contacts);
-  }
+  // const deleteContact = (id) => {
+  //   const newContacts = contacts.filter(contact => contact.id !== id);
+  //   setContacts(newContacts);
+  // }
+  // const fomSubmitHeader = (contact) => {
+  //   const newContact = {
+  //     id: nanoid(),
+  //     name: contact.name,
+  //     number: contact.number,
+  //   };
+  //   setContacts([...contacts, newContact]);
+  //   console.log(contacts);
+  // }
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  } , [contacts]);
+  // useEffect(() => {
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // } , [contacts]);
 
 
   return (
@@ -45,20 +52,20 @@ const App = () => {
       <div className={css.header}>
         <h1 className={css.title}>Phonebook</h1>
         <ContactsForm
-          onSubmit={fomSubmitHeader}
+          // onSubmit={fomSubmitHeader}
           contacts={contacts}
-          deleteContact={deleteContact}
+          // deleteContact={deleteContact}
         />
        
         <h2 className={css.title}>Contacts</h2>
         <Filter
           filter={filter}
-          onFilterChange={onFilterChange}
+          // onFilterChange={onFilterChange}
         />
         <ContactsList
           contacts={contacts}
           filter={filter}
-          deleteContact={deleteContact}
+          // deleteContact={deleteContact}
        />
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
 
+/*REDUX*/
+import store from 'redux/store';
+import { Provider } from 'react-redux';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
+
+console.log(store.getState());
