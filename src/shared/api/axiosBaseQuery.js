715f0edcea3e9564ac78464ac07039643237884b
Aleksandr Pavlov <pavlovaleksandr@ukr.net>
2022-10-24 14:24:00 +0300

    Update axiosBaseQuery.js

--- src/shared/api/axiosBaseQuery.js
+++ src/shared/api/axiosBaseQuery.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosBaseQuery =
-  ({ baseUrl } = { baseUrl: 'https://connections-api.herokuapp.com' }) =>
+  ({ baseUrl } = { baseUrl: 'https://phonebook-api-pav.herokuapp.com' }) =>
   async ({ url, method, data }, { getState }) => {
     try {
       const token = getState().token;
